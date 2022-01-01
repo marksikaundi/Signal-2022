bd2f01ad2c5e36b5d5d195b45e1ba5cc3e6e76d5
marksikaundi <emmanuelmark117@gmail.com>
2022-01-01 14:28:17 +0200

    firebase installations

--- firebase.js
+++ firebase.js
@@ -1,3 +1,7 @@
+import * as firebase from "firebase";
+import "firebase/auth";
+import "firebase/firestore";
+
 const firebaseConfig = {
     apiKey: "AIzaSyBbeBKIdkxTo_dF-zcF53yQ3a_7eEqvrJU",
     authDomain: "signal-6dae6.firebaseapp.com",
@@ -6,3 +10,12 @@ const firebaseConfig = {
     messagingSenderId: "348126845363",
     appId: "1:348126845363:web:aac2aef3422ca38f9bd3c6"
   };
+
+  let app;
+  
+  if (firebase.apps.length === 0) {
+    app = firebase.initializeApp(firebaseConfig)
+  } else {
+    app = firebase.app();
+  }
+
