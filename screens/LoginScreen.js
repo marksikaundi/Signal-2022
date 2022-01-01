f2a94b78d33506d7e15f0cc0d3a51c928aec0e44
marksikaundi <emmanuelmark117@gmail.com>
2022-01-01 12:38:21 +0200

    file clean

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,14 +1,19 @@
 import React, { useState } from "react";
-import { StyleSheet, Text, View } from "react-native";
-import { Button, Input, Image } from "react-native-elements";
+import { StyleSheet, Text, View, KeyboardAvoidingView } from "react-native";
+import { Button, Input, Image, withTheme } from "react-native-elements";
 import { StatusBar } from "expo-status-bar";
+// import { KeyboardAvoidingView } from "react-native";
 
 const LoginScreen = () => { 
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
 
+    const signIn = () => {
+
+    }
+
     return (
-        <View>
+        <KeyboardAvoidingView behavior="padding" style={styles.container}>
             <StatusBar style="light" />
             <Text>Login Screen</Text>
 
@@ -35,12 +40,26 @@ const LoginScreen = () => {
  
 
             </View>
-        </View>
+            <Button containerStyle={styles.button} onPress={signIn} title="Login" />
+            <Button containerStyle={styles.button} type="outline" title="Register" />
+            <View style={{ height: 100 }} />
+        </KeyboardAvoidingView>
     );
 };
 
 export default LoginScreen
 
 const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "center",
+        padding: 10,
+        backgroundColor: "white",
+    },
     inputContainer: {},
+    button: {
+        width: 200,
+        marginTop: 10,
+    }
 });
