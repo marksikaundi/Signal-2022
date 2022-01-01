cc22b7c132900edac5f30e23430da3ec319b5621
marksikaundi <emmanuelmark117@gmail.com>
2022-01-01 13:08:35 +0200

    Register Screen added

--- screens/RegisterScreen.js
+++ screens/RegisterScreen.js
@@ -1,14 +1,24 @@
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, KeyboardAvoidingView } from 'react-native'
+import { StatusBar } from 'expo-status-bar';
 
 const RegisterScreen = ({ navigation }) => {
     return (
-        <View>
+        <KeyboardAvoidingView behavior="padding" style={styles.container}>
+            <StatusBar style="light" />
             <Text>Hey Register Here</Text>
-        </View>
+
+            <Text h3 style={{ marginBottom: 50 }}>
+                Create a Signal account
+            </Text>
+        </KeyboardAvoidingView>
     )
 }
 
 export default RegisterScreen
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    container: {
+
+    }
+})
